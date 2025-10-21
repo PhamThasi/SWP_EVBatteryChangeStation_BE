2b71f80bb1630534468a9a5083f45799ca362668
PhamThasi <qthai24042004@gmail.com>
2025-10-21 23:00:21 +0700

    vnpay

---
+++ EV_BatteryChangeStation_Common/DTOs/VNPayDTO/VNPayConfigDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV_BatteryChangeStation_Common.DTOs.VNPayDTO
+{
+    public class VNPayConfigDto
+    {
+        public string TmnCode { get; set; } = null!;
+        public string HashSecret { get; set; } = null!;
+        public string ReturnUrl { get; set; } = null!;
+        public string PaymentUrl { get; set; } = null!;
+    }
+}
