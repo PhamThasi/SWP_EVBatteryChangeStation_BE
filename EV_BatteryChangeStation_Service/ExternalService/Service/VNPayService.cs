ed54edb43e46e651a2a7f80baaa43d11f548a848
PhamThasi <qthai24042004@gmail.com>
2025-10-23 13:49:24 +0700

    add vnpay service

---
+++ EV_BatteryChangeStation_Service/ExternalService/Service/VNPayService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EV_BatteryChangeStation_Service.ExternalService.Service
+{
+    internal class VNPayService
+    {
+    }
+}
